8456fb0902ee6bf5ae343591e0dbe5c1c4bdb821
memelo <casi_nsn@hotmail.com>
2017-10-26 21:36:27 -0500

    navbar created

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'galeria', component: GalleryComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: 'acceso', component: LoginComponent },
+  { path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+	imports: [
+	    BrowserModule,
+	    RouterModule.forRoot(
+	      appRoutes,
+	      { enableTracing: true }
+	    )
+  	],
+  	declarations: [
+	    AppComponent,
+	    HomeComponent,
+	    LoginComponent,
+	    ContactComponent,
+	    GalleryComponent
+  	],
+  	providers: [],
+  	bootstrap: [AppComponent]
+})
+export class AppModule { }
